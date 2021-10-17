f352dab01b7d9c6e557eaecbeb494fb0a14d43c4
Connor-Mai <37640393+Connor-Mai@users.noreply.github.com>
2021-10-17 22:19:26 +1100

    Add files via upload

--- asssignment2_Vis.js
+++ asssignment2_Vis.js
@@ -1,9 +1,9 @@
 var vg_1 = "connectjson.vg.json";
-vegaEmbed("#vis1", vg_1).then(function(result) { 
+vegaEmbed("#vis1", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
 
 var vg_2 = "splomjson.vg.json";
-vegaEmbed("#vis2", vg_2).then(function(result) { 
+vegaEmbed("#vis2", vg_2).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
